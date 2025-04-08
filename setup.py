ccf015f79fc3d8595d868ae43beb842471f97055
DavidP <148255648+DavidP0011@users.noreply.github.com>
2025-04-08 14:31:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8") if (here / "README.md").exists() else ""
 
 setup(
-    name='dpm_utils_functions',
+    name='dpm_common_functions',
     version='0.1',
     description='Conjunto de funciones de utilidades varias',
     url='https://github.com/DavidP0011/etl_functions',
